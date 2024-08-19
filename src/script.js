53282c7b8744798bc5d15af8668f104d72b01289
hayapy88 <payagram01@gmail.com>
2024-08-19 22:52:57 +1000

    Set up Prisma Client

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "Hello GraphQL",
+      url: "https://graphql.org/",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    prisma.$disconnect;
+  });
