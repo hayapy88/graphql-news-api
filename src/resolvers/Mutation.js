6f0ab69f571aa4e0b0c7285edf9d8b0bc6c2b5e9
hayapy88 <payagram01@gmail.com>
2024-08-23 06:29:17 +1000

    Create resolver for signUp

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,14 +1,35 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// This JWT secret key is used to sign and verify JSON Web Tokens
+const JWT_SECRET = process.env.JWT_SECRET;
 
 async function signUp(parent, args, context) {
-  // Set password
-  const password = await bcrypt.hash(args.password, 10);
+  try {
+    // Set password
+    const password = await bcrypt.hash(args.password, 10);
 
-  // Create a user
-  const user = await context.prisma.user.create({
-    data: {
-      ...args,
-      password,
-    },
-  });
+    // Create a user
+    const user = await context.prisma.user.create({
+      data: {
+        ...args,
+        password,
+      },
+    });
+
+    // Set a payload
+    const payload = { userId: user.id };
+
+    // Generate JWT token
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+    return {
+      token,
+      user,
+    };
+  } catch (error) {
+    console.error("Error signing up user:", error);
+    throw new Error(
+      "An error occurred while signing up. Please try again later."
+    );
+  }
 }
