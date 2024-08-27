48ff94ad9911a80cf3ac0f605ffdd4b0acc6c45b
hayapy88 <payagram01@gmail.com>
2024-08-27 21:13:41 +1000

    Fix code in Link.js

--- src/resolvers/Link.js
+++ src/resolvers/Link.js
@@ -1,9 +1,7 @@
 function postedBy(parent, args, context) {
-  return context.prisma.link
-    .findUnique({
-      where: { id: parent.id },
-    })
-    .postedBy();
+  return context.prisma.link.findUnique({
+    where: { id: parent.id },
+  }).postedBy;
 }
 
 module.exports = {
